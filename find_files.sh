bd96b6a1ebd32d2386b55a5d1f919edfe4610c57
Jordan Bray <jordan@jordanbray.com>
2025-06-23 23:19:59 -0400

    Document why these scripts exist.

--- find_files.sh
+++ find_files.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/bash
 
-find target/release/build/sqisign-rust-97635a8a0ce3ad20/out/build/src/ | sed 's/.*build/build/' | grep '\.a' | sed '/test/d' | sed 's/.*\/lib//' | sed 's/\.a$//' | sed 's/^/println!("cargo:rustc-link-lib=static:+whole-archive=/' | sed 's/$/");/'
+# You probably don't need this code.  It is only here for documentation
+# purposes.  The `build.rs` file has already been made, and is in the
+# repository.
+# 
+# This script finds all the `.a` files associated with the built
+# version of this project (after a `cargo build --release`), and then
+# reformats the text to be compatibile with the `build.rs` file rust
+# wants.
+#
+# WARNING: this does _not_ find the GMP library.  If you build your
+# own `build.rs`, you will need to manually add GMP somehow.
+
+find target/release/build/sqisign-rust-*/out/build/src/ |
+  sed 's/.*build/build/' |
+  grep '\.a' |
+  sed '/test/d' |
+  sed 's/.*\/lib//' |
+  sed 's/\.a$//' |
+  sed 's/^/println!("cargo:rustc-link-lib=static:+whole-archive=/' |
+  sed 's/$/");/'
--- find_folders.sh
+++ find_folders.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/bash
 
-find target/release/build/sqisign-rust-*/out/build/src/ | sed 's/.*build/\/build/' | grep '\.a' | sed '/test/d' | sed 's/\/[a-z_.0-9]*$//' | sort | uniq | sed 's/^/println!("cargo:rustc-link-search=native={}/' | sed 's/$/", dst.display());/'
+# You probably don't need this code.  It is only here for documentation
+# purposes.  The `build.rs` file has already been made, and is in the
+# repository.
+# 
+# This script finds all the folders that the various `.a` files
+# ended up in, so that the linker can locate them all.  It then
+# outputs them in a way that is compatible with the `build.rs` file
+# from rust.
+#
+# 
+# WARNING: this does _not_ find the GMP library.  If you build your
+# own `build.rs`, you will need to manually add GMP somehow.
+
+find target/release/build/sqisign-rust-*/out/build/src/ |
+  sed 's/.*build/\/build/' |
+  grep '\.a' |
+  sed '/test/d' |
+  sed 's/\/[a-z_.0-9]*$//' |
+  sort |
+  uniq |
+  sed 's/^/println!("cargo:rustc-link-search=native={}/' |
+  sed 's/$/", dst.display());/'
